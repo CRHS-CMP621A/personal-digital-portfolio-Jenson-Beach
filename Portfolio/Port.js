6516eea9cf4b72c21c7a8a81e3a5076caabdea33
Jenson-Beach <jebe0068@cloud.edu.pe.ca>
2022-06-07 01:56:35 -0300

    156am changes

--- Portfolio/Port.js
+++ Portfolio/Port.js
@@ -1,4 +1,14 @@
-function green(){
-    alert("hi")
+function instagramLink(){
+    window.open('https://www.instagram.com', '_blank');
 }
-document.querySelector(".navi").addEventListener("click", green)    
+
+function snapchatLink(){
+    window.open('https://www.snapchat.com', '_blank');
+}
+
+function discordLink(){
+    window.open('https://discord.com', '_blank');
+}
+document.querySelector(".instagram").addEventListener("click", instagramLink)
+document.querySelector(".snapchat").addEventListener("click", snapchatLink)
+document.querySelector(".discord").addEventListener("click", discordLink)
